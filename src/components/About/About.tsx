7ada8862d83ce28810ed5b02ee2f5442ec688641
ATISHEK SINGH <atishek.singh@techalchemy.com>
2024-10-10 00:29:16 +0530

    initial commit

---
+++ src/components/About/About.tsx
@@ -0,0 +1,97 @@
+import { Container } from "./styles";
+import atishek from "../../assets/atishek.jpeg";
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+          A blockchain developer with a passion for building decentralized applications. Proficient in Solidity and Layer 2 scaling solutions, with a deep understanding of the Ethereum ecosystem.</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+          Also proficient in utilizing Web3 and DeFi protocols, with experience integrating blockchain solutions into existing platforms. This allows clients to easily manage and update their websites and applications with the added security and transparency of blockchain technology.</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+          If you're looking to build a cutting-edge decentralized application or revamp your online presence with blockchain technology, I'd love to discuss your project. Let's bring your Web3 vision to life!</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
+              <img src={wordpress} alt="Wordpress" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <img src={shopify} alt="shopify" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={typescriptIcon} alt="Typescript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
+              <img src={vueIcon} alt="Vue" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={boostrapIcon} alt="bootstrap" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+          <img src={atishek} alt="atishek singh" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,62 @@
+import { Container } from './styles'
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+// import discordIcon from '../../assets/discord.png'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <a href="www.linkedin.com/in/atishek1" className="logo">
+        <span>www.atishek</span>
+        <span>singh.com</span>
+      </a>
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/atisheks"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/atisheksingh"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=%2B918299634308&text=Hello+Atishek"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={whatsapp} alt="Whatsapp" />
+        </a>
+        <a
+          href="https://t.me/SolidityMonk"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/atishek1/?hl=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+      </div>
+    </Container>
+  )
+}
